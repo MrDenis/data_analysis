128fd1179e61d8f5355c18c3a2d800dd49d4cc26
MrDenis <denys.shapovalov@gmail.com>
2022-04-27 18:41:09 +0300

    add filters

---
+++ analytics/filters/3k_poznyaki.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+filter_values = { 
+    'cat3' : [
+        '103: Квартири 3-кімнатні'
+    ],
+    'cat4' : [
+        'Київ. Дарницький р-н.'
+    ],
+    'new_build': ['Y', 'N', ''],
+    'sq_total_max' : 115,
+    'price_per_1m2_usd' : 1200,
+    'price_per_1m2_uah' : 30000,
+    'price_usd': 150000,
+    'price_uah': 4000000
+}
+
+
